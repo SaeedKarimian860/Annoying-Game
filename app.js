8cc002afd1532142f70e487dbbb2d843712f3c3c
SaeedKarimian860 <SGK12190@gmail.com>
2024-10-26 19:31:10 -0400

    multiple events/event object/key events

--- app.js
+++ app.js
@@ -22,17 +22,81 @@
 
 
 
-const btn = document.querySelector('button')
+//Code for Annoying Button Game
+// const btn = document.querySelector('button')
 
-btn.addEventListener('mouseover', function () {
-    console.log('MOUSED OVER ME!');
-    const height = Math.floor(Math.random() * window.innerHeight);
-    const width = Math.floor(Math.random() * window.innerWidth);
-    btn.style.left = `${width}px`;
-    btn.style.top = `${height}px`;
+// btn.addEventListener('mouseover', function () {
+//     console.log('MOUSED OVER ME!');
+//     const height = Math.floor(Math.random() * window.innerHeight);
+//     const width = Math.floor(Math.random() * window.innerWidth);
+//     btn.style.left = `${width}px`;
+//     btn.style.top = `${height}px`;
+// });
+
+// btn.addEventListener('click', function () {
+//     btn.innerText = 'YOU GOT ME!';
+//     document.body.style.backgroundColor = 'green';
+// });
+
+
+
+//Code for multiple elements/event object
+//const colors = [
+//   "red",
+//   "orange",
+//   "yellow",
+//   "green",
+//   "blue",
+//   "purple",
+//   "indigo",
+//   "violet",
+// ];
+// const h1 = document.querySelector("h1");
+// const changeColor = function (evt) {
+//   h1.style.color = this.style.backgroundColor;
+// };
+// const container = document.querySelector("#boxes");
+
+// for (let color of colors) {
+//   const box = document.createElement("div");
+//   box.style.backgroundColor = color;
+//   box.classList.add("box");
+//   container.appendChild(box);
+//   box.addEventListener("click", changeColor);
+// }
+
+// document.body.addEventListener('keypress', function (e) {
+//     console.log(e);
+// });
+
+
+
+
+const input = document.querySelector('#username');
+
+input.addEventListener('keydown', function (e) {
+    console.log('KEY DOWN!');
 });
 
-btn.addEventListener('click', function () {
-    btn.innerText = 'YOU GOT ME!';
-    document.body.style.backgroundColor = 'green';
+input.addEventListener('keyup', function (e) {
+    console.log('KEY UP!');
+});
+
+input.addEventListener('keypress', function (e) {
+    console.log('KEY PRESS!');
+});
+
+const addItemInput = document.querySelector('#addItem');
+const itemsUl = document.querySelector('#items');
+
+addItemInput.addEventListener('keypress', function(e){
+    if (e.key === 'Enter') {
+        if (!this.value) return;
+        //add a new item to list
+        const newItemText = this.value;
+        const newItem = document.createElement('li');
+        newItem.innerText = newItemText;
+        itemsUl.appendChild(newItem);
+        this.value = '';
+    }
 });
